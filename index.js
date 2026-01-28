8b8bb18d9504459dba0e9e964a421c9a12989ee0
Abhishek <abhisheksinghnegi862@gmail.com>
2026-01-29 02:28:59 +0530

    sub() method 2 commits

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function add(a,b){
     console.log(a+b);
 }
+
+function subtract(x,y){
+    if(!(a>b)) throw new Error("num a should be greater than num b")
+    return x-y
+}
 add(2,3)
 git(6,44)
-
+subtract(3,4)
