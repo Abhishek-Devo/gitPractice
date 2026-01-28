fc1c965f442fac5a820ed21caa07bec049327320
Abhishek-Devo <122733700+Abhishek-Devo@users.noreply.github.com>
2026-01-29 03:13:06 +0530

    Feat/multiply (#1)
    * multiply method add
    
    * use multiply method
    
    * use multiply 2nd time
    
    * use multiply 3rd time
    
    * use multiply 4th time
    
    * added checking in multiply

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ function subtract(x,y){
     if(!(a>b)) throw new Error("num a should be greater than num b")
     return x-y
 }
+function multiply(a,b){
+    if(a<0) throw new Error("num greater than 0 only allowed")
+    return a*b
+}
+
 add(2,3)
 git(6,44)
 subtract(3,4)
+multiply(6,3)
+multiply(34,5)
+multiply(44,44)
+multiply(2,4)
